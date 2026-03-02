48487a765890ad7da300a816ad3520cd09ee48d9
Guilherme Miranda Begnini <75757228+gui1232020@users.noreply.github.com>
2026-03-02 19:56:54 -0300

    feat: Update the .py file
    Update the .py file for silver layer with new script.

---
+++ src/2.silver/transform_silver.py
@@ -0,0 +1,48 @@
+import pandas as pd
+
+file = '../../data/2.bronze/ingest_bronze.parquet'
+
+df_file = pd.read_parquet(file)
+
+df = pd.DataFrame(df_file)
+
+# df
+
+df_step = df.copy()
+
+df_step = df_step.explode('assignee')
+df_step = df_step.explode('timestamps')
+
+assignee_colunms = pd.json_normalize(df_step['assignee'])
+timestamps_colunms = pd.json_normalize(df_step['timestamps'])
+
+df = pd.concat(
+    [
+        df_step.drop(columns=['assignee', 'timestamps']),
+        assignee_colunms,
+        timestamps_colunms
+    ],
+    axis=1
+)
+
+# df
+
+df['created_at'] = df['created_at'].replace('2026-02-30T25:61:00Z','2026-02-28T23:59:59Z')
+df['resolved_at'] = df['resolved_at'].replace('not_a_date',None)
+
+# df['created_at'].value_counts()
+
+# df
+
+df['created_at'] = pd.to_datetime(df['created_at'], utc=True)
+df['resolved_at'] = pd.to_datetime(df['resolved_at'], utc=True)
+
+# df
+
+df.columns = ['issue_id','issue_type','issue_status','issue_priority','assignee_email','assignee_id','assignee_name','issue_created_at','issue_resolved_at']
+
+# df.head(5)
+
+# df
+
+df.to_parquet("../../data/3.silver/transform_silver.parquet", index=False)
